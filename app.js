0a40ac7582eeaead06ac1ed8feeb4973dbf295e7
gjackso4 <gregoryljackson@gmail.com>
2021-10-29 10:07:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,35 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log(req);
+  res.json({message: "Thinkiry API"});
 });
 
-app.get('/api/email', (req, res) => {
-  res.json({email: "TEST123"});
+
+// Send a GET request to /api/emails to READ a list of emails
+app.get('/api/emails', (req, res) => {
+
+  res.json({data});
+});
+
+//Send a GET request to /api/emails/:id
+app.get('/api/emails/:id', (req, res) => {
+  const email = data.emails.find(quote => quote.id == req.params.id);
+  res.json({email});
 });
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
+
+
+const data = {
+  emails: [
+    {
+      id: 1
+    },
+    {
+      id: 2
+    },
+    {
+      id: 3
+    }
+  ]
+}
