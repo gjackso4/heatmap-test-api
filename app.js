21a8080d0d191ef98d507775c3f5a65fba02f0fa
gjackso4 <gregoryljackson@gmail.com>
2021-10-29 09:28:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.get('/api/email', (req, res) => {
+  res.json({email: "TEST123"});
+});
+
 app.listen(3001, () => console.log('Quote API listening on port 3001!'));
